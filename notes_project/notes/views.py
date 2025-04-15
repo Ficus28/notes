06fe93a2e0d446397db9d50e62f9be635c20e33e
Ficus28 <sutniktv@gmail.com>
2025-04-15 23:08:43 +0300

    Сделал модели, миграции, базавую админку, маршруты и функции. реализовал минимальный функционал веб-приложения

--- notes_project/notes/views.py
+++ notes_project/notes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Note
 
-# Create your views here.
+def home(request):
+    notes = Note.objects.all().order_by('-created_at')
+    return render(request, 'notes/home.html', {'notes': notes})
+
+def add_note(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        Note.objects.create(title=title, content=content)
+        return redirect('home')
+    return render(request, 'notes/add_note.html')

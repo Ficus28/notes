9c41e1253a88fb003c27346d4e6f6516e3d5b44a
Ficus28 <sutniktv@gmail.com>
2025-04-16 20:42:35 +0300

    Добавил регистрации, вход и выход из провфиля, пока без привязки к бд

--- notes_project/notes/urls.py
+++ notes_project/notes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add/', views.add_note, name='add_note'),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- notes_project/notes/views.py
+++ notes_project/notes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Note
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     notes = Note.objects.all().order_by('-created_at')
@@ -17,18 +17,18 @@ def add_note(request):
 
 def register_view(request):
     if request.method == 'POST':
-        username = request.POST['login']
+        username = request.POST['username']
         password = request.POST['password']
+        if User.objects.filter(username=username).exists():
+            return render(request, 'notes/register.html', {'error': 'Логин уже занят'})
         user = User.objects.create_user(username=username, password=password)
         login(request, user)
         return redirect('home')
     return render(request, 'notes/register.html')
 
-from django.contrib.auth import authenticate, login
-
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST['login']
+        username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
@@ -37,3 +37,7 @@ def login_view(request):
         else:
             return render(request, 'notes/login.html', {'error': 'Неверный логин или пароль'})
     return render(request, 'notes/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')

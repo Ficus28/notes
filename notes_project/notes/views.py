faa349011ab6eac818cdc7c5f255f2bffeb9bb0f
Ficus28 <sutniktv@gmail.com>
2025-04-16 21:14:02 +0300

    добавил возможность пользователю при регистрации указывать почту

--- notes_project/notes/views.py
+++ notes_project/notes/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     notes = Note.objects.all().order_by('-created_at')
-    return render(request, 'notes/home.html', {'notes': notes})
+    return render(request, 'home.html', {'notes': notes})
 
 def add_note(request):
     if request.method == 'POST':
@@ -13,18 +13,22 @@ def add_note(request):
         content = request.POST['content']
         Note.objects.create(title=title, content=content)
         return redirect('home')
-    return render(request, 'notes/add_note.html')
+    return render(request, 'add_note.html')
 
 def register_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+        email = request.POST['email']
+
         if User.objects.filter(username=username).exists():
-            return render(request, 'notes/register.html', {'error': 'Логин уже занят'})
-        user = User.objects.create_user(username=username, password=password)
+            return render(request, 'register.html', {'error': 'Логин уже занят'})
+
+        user = User.objects.create_user(username=username, password=password, email=email)
         login(request, user)
         return redirect('home')
-    return render(request, 'notes/register.html')
+
+    return render(request, 'register.html')
 
 def login_view(request):
     if request.method == 'POST':
@@ -35,8 +39,8 @@ def login_view(request):
             login(request, user)
             return redirect('home')
         else:
-            return render(request, 'notes/login.html', {'error': 'Неверный логин или пароль'})
-    return render(request, 'notes/login.html')
+            return render(request, 'login.html', {'error': 'Неверный логин или пароль'})
+    return render(request, 'login.html')
 
 def logout_view(request):
     logout(request)

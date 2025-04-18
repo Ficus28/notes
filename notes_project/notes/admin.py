04eb71604973e2c0db8e07e9b2272b83eb9bc523
Ficus28 <sutniktv@gmail.com>
2025-04-18 23:25:55 +0300

    доработал админку, теперь отображается когда пользователь входит\выходит из аккаунта

--- notes_project/notes/admin.py
+++ notes_project/notes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Note
+from .models import UserSessionLog
 
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created_at') 
@@ -12,3 +13,9 @@ class NoteAdmin(admin.ModelAdmin):
         return qs.filter(user=request.user)
 
 admin.site.register(Note, NoteAdmin)
+
+@admin.register(UserSessionLog)
+class UserSessionLogAdmin(admin.ModelAdmin):
+    list_display = ('user', 'login_time', 'logout_time')
+    list_filter = ('user',)
+    ordering = ('-login_time',)
